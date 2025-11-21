082d11759e604e7a7ec074207d9ef47fd80adabb
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-11-21 07:28:49 -0300

    Comandos de DDL

---
+++ ddl_commands.sql
@@ -0,0 +1,16 @@
+-- DDL | Data Definition Language
+
+
+-- Criar
+CREATE TABLE ddl_example (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50),
+	sexo VARCHAR(1)
+);)
+
+-- Alterar
+ALTER TABLE ddl_example
+ADD email VARCHAR(50);)
+
+-- Deletar
+DROP TABLE ddl_example;
