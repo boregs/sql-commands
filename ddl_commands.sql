b454ef6b8b30780ab8a4930d4acca46d0d620dcf
Rafael Borges <rafaborgesdasilva75@gmail.com>
2025-11-22 21:32:35 -0300

    Fix syntax errors in ddl_commands.sql

--- ddl_commands.sql
+++ ddl_commands.sql
@@ -6,11 +6,11 @@ CREATE TABLE ddl_example (
 	id SERIAL PRIMARY KEY,
 	nome VARCHAR(50),
 	sexo VARCHAR(1)
-);)
+);
 
 -- Alterar
 ALTER TABLE ddl_example
-ADD email VARCHAR(50);)
+ADD email VARCHAR(50);
 
 -- Deletar
 DROP TABLE ddl_example;
